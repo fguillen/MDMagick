73341bed80dd12ae05bbb604ec2c7940d8009083
Fernando Guillen <fguillen.mail@gmail.com>
2012-08-03 18:01:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# MDMagick
+
+Add MarkDown butttons and preview to any kind of input field.
+
+## Dependencies
+
+* jquery
+* a-tools
+* showdown
+
+## License
+
+Creative Commons BY
+
+## Attributions
+
+* Icons used in the control div from: [IcoMoon](http://keyamoon.com/icomoon/#toHome)
---
+++ etc/icon_font/lte-ie7.js
@@ -0,0 +1,30 @@
+/* Use this script if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-bold' : '&#x21;',
+			'icon-font-size' : '&#x22;',
+			'icon-italic' : '&#x23;',
+			'icon-underline' : '&#x24;',
+			'icon-list' : '&#x26;',
+			'icon-out' : '&#x25;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^s'"]+/);
+		if (c) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
